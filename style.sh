c71b37e5acd65cec2d8936f01d1eec4ee671b897
Andrey Valyaev <dron.valyaev@gmail.com>
2018-04-22 20:56:07 +0300

    #1: Test skeleton

--- style.sh
+++ style.sh
@@ -4,8 +4,8 @@ set -ue
 set -v
 
 # Check coding style
-cppcheck -I. --enable=all --quiet *.cpp
-cpplint *.cpp
+cppcheck -I. --enable=all --quiet *.cpp test/*.cpp
+cpplint *.cpp test/*.cpp
 
 # Check pdd tokens
 pdd > /dev/null
