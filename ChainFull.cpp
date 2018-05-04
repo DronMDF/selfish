6f406e9143b90c24bc1e3f63cb49211a7065dc81
Andrey Valyaev <dron.valyaev@gmail.com>
2018-05-04 23:13:58 +0300

    #9: ChainFull implementation

---
+++ ChainFull.cpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2018 Andrey Valyaev <dron.valyaev@gmail.com>
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+
+#include "ChainFull.h"
+
+using namespace std;
+
+ChainFull::ChainFull(
+	const shared_ptr<const Chain> &chain,
+	const list<shared_ptr<const Block>> &blocks
+) : chain(chain), blocks(blocks)
+{
+}
+
+vector<shared_ptr<const Block>> ChainFull::heads() const
+{
+	return {blocks.begin(), blocks.end()};
+}
