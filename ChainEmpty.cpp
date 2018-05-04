4536186f78494f314a34ecdd897f5d2adb13efc0
Andrey Valyaev <dron.valyaev@gmail.com>
2018-05-04 23:32:29 +0300

    #21: Rename EmptyChain to ChainEmpty

--- ChainEmpty.cpp
+++ ChainEmpty.cpp
@@ -3,12 +3,12 @@
 // This software may be modified and distributed under the terms
 // of the MIT license.  See the LICENSE file for details.
 
-#include "EmptyChain.h"
+#include "ChainEmpty.h"
 #include "BlockNull.h"
 
 using namespace std;
 
-vector<shared_ptr<const Block>> EmptyChain::heads() const
+vector<shared_ptr<const Block>> ChainEmpty::heads() const
 {
 	return { make_shared<BlockNull>() };
 }
